29fda61d728e240d0bbd550a64947509f0b71d0d
Nikoll Bonilla Hancco <nikoll.bonilla@tecsup.edu.pe>
2021-11-26 12:11:16 -0500

    Laboratorio terminado

--- lab14/backend/backend/urls.py
+++ lab14/backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""lab01 URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import path,include
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
-    path('polls/', include('encuesta.urls')),
-    path('app/', include('app.urls')),
+    path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
